01303c61d49ff69479ae1d02754f8d4a52355fe1
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-03-29 14:57:15 +0200

    add BE project

---
+++ backend/src/test/kotlin/hu/hollo/news/NewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.hollo.news
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
