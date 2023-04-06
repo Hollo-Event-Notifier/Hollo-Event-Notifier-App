c8c6d4457c58e73fee3869c350fed245cec32fe4
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-06 21:22:15 +0200

    created login standalone component an necessary models, services

---
+++ frontend/src/app/features/login/services/user.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {SnackbarService} from "../../../core/services/snackbar.service";
+import {Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
+import {UserApiService, UserCredentialsDto} from "../../../core/api";
+import {AppRoutes} from "../../../app-routes";
+import {take} from "rxjs";
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    private userApi: UserApiService,
+    private snackBar: SnackbarService,
+    private router: Router
+  ) {
+  }
+
+  loginUser(credentials: UserCredentialsDto): void {
+    this.userApi.loginUser(credentials)
+      .pipe(take(1))
+      .subscribe({
+      next: () => this.router.navigate([AppRoutes.Admin]).then(),
+      error: (err: HttpErrorResponse) => this.snackBar.open(err?.error)
+    });
+  }
+}
