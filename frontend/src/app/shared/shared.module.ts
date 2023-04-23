6e19024f6dbf2d20ce9a9ed7fdc949dad4483afd
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-23 20:06:35 +0200

    add directives imports

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,17 +1,24 @@
 import {NgModule} from '@angular/core';
 import {MaterialModule} from "./material.module";
 import {ReactiveFormsModule} from "@angular/forms";
-import {NgStyle} from "@angular/common";
+import {NgIf, NgStyle} from "@angular/common";
+import {FullCalendarModule} from '@fullcalendar/angular';
+
+
+const DIRECTIVES = [
+  NgStyle,
+  NgIf,
+];
 
 const MODULES = [
   MaterialModule,
   ReactiveFormsModule,
-  NgStyle
+  FullCalendarModule
 ]
 
 @NgModule({
-  imports: [...MODULES],
-  exports: [...MODULES]
+  imports: [...MODULES, ...DIRECTIVES],
+  exports: [...MODULES, ...DIRECTIVES]
 })
 export class SharedModule {
 }
