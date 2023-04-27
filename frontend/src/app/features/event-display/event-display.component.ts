44fdf85499c350a097079968ec9694820c7a3e53
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-27 12:45:32 +0200

    added full-calendar to event-display.component

--- frontend/src/app/features/event-display/event-display.component.ts
+++ frontend/src/app/features/event-display/event-display.component.ts
@@ -1,7 +1,16 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {SharedModule} from "../../shared/shared.module";
-import {repeat} from "rxjs";
-import {NgStyle} from "@angular/common";
+import {CalendarOptions, DateSelectArg, EventApi, EventClickArg, EventInput} from "@fullcalendar/core";
+import {createEventId, INITIAL_EVENTS} from "./utils/event-utils";
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import {EventDto} from "../../core/api";
+import {ApplicationStateService} from "../../core/services/application-state.service";
+import {EventsService} from "./services/events.service";
+import {Observable, Subject, takeUntil} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-event-display',
@@ -10,69 +19,75 @@ import {NgStyle} from "@angular/common";
   standalone: true,
   imports: [
     SharedModule,
+    AsyncPipe,
   ],
 })
-export class EventDisplayComponent {
-  private readonly days = [
-    'monday',
-    'tuesday',
-    'wednesday',
-    'thursday',
-    'friday',
-    'saturday',
-    'sunday'
-  ];
-  private readonly intervalls = [
-    '0-1',
-    '1-2',
-    '2-3',
-    '3-4',
-    '4-5',
-    '5-6',
-    '6-7',
-    '7-8',
-    '8-9',
-    '9-10',
-    '10-11',
-    '11-12',
-    '12-13',
-    '13-14',
-    '14-15',
-    '15-16',
-    '16-17',
-    '17-18',
-    '18-19',
-    '19-20',
-    '20-21',
-    '21-22',
-    '22-23',
-    '23-24',
-  ]
+export class EventDisplayComponent implements OnInit, OnDestroy {
+  private readonly unsubscribe$ = new Subject<void>();
+  events$!: Observable<EventInput[]>;
 
-  containerStyle: Record<string, string> = {
-    'display': 'grid',
-    'grid-template-columns': 'auto repeat(7, 1fr)',
-    'grid-template-rows': 'repeat(24, 1fr)',
-    'gap': '5px 5px',
-    'grid-auto-flow': 'row',
-    'grid-template-areas': this.generateAreas()
+  calendarOptions: CalendarOptions = {
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'timeGridWeek',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  };
+
+  constructor(
+    private readonly changeDetector: ChangeDetectorRef,
+    private readonly state: ApplicationStateService,
+    private readonly eventsService: EventsService
+  ) {
+    this.events$ = this.state.events;
   }
 
-  private generateAreas(): string {
-    let areas: Array<Array<string>>;
+  ngOnInit(): void {
+    this.eventsService.getEvents(new Date('2023-04-23T00:00:00.000Z'), new Date('2023-04-29T00:00:00.000Z'))
+  }
 
-    // areas
-    //
-    // for (let i = 0; i < ; i++) {
-    //
-    // }
-    //
-    // for (let i = 0; i < 8; i++) {
-    //   for (let j = 0; j < 24; j++) {
-    //     areas
-    //   }
-    // }
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
-    return 'areas.toString()';
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    alert('Event clicked!');
   }
 }
