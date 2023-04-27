634210f9880a8fdb9daed0ae12614f026254d764
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-28 00:08:12 +0200

    changed state imports

--- frontend/src/app/core/models/application-state.ts
+++ frontend/src/app/core/models/application-state.ts
@@ -1,5 +1,4 @@
-import {EventDto} from "../api";
-import {EventApi, EventInput} from "@fullcalendar/core";
+import {EventInput} from "@fullcalendar/core";
 
 export interface ApplicationState {
   events: EventInput[]
