c265edbfa155bfcd301aa87493586014268bcde5
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-27 12:44:58 +0200

    added events to app state

--- frontend/src/app/core/models/application-state.ts
+++ frontend/src/app/core/models/application-state.ts
@@ -1,2 +1,6 @@
+import {EventDto} from "../api";
+import {EventApi, EventInput} from "@fullcalendar/core";
+
 export interface ApplicationState {
+  events: EventInput[]
 }
--- frontend/src/app/core/services/application-state.service.ts
+++ frontend/src/app/core/services/application-state.service.ts
@@ -1,11 +1,23 @@
 import {Injectable} from '@angular/core';
 import {ApplicationState} from "../models/application-state";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, map, Observable} from "rxjs";
+import {EventDto} from "../api";
+import {EventApi, EventInput} from "@fullcalendar/core";
 
 @Injectable()
 export class ApplicationStateService {
 
-  private state: BehaviorSubject<ApplicationState> = new BehaviorSubject<ApplicationState>({});
+  private state: BehaviorSubject<ApplicationState> = new BehaviorSubject<ApplicationState>({
+    events: []
+  });
+
+  get events(): Observable<EventInput[]> {
+    return this.state
+      .asObservable()
+      .pipe(
+        map(state => state.events)
+      );
+  }
 
   patchState(newState: Partial<ApplicationState>) {
     this.state.next({
