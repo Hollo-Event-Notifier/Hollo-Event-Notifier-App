883b0326bc9b9f36234990554498d93d51fe4666
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-28 00:09:39 +0200

    changed events.service to use mapper

--- frontend/src/app/features/event-display/services/events.service.ts
+++ frontend/src/app/features/event-display/services/events.service.ts
@@ -1,31 +1,26 @@
 import {Injectable} from '@angular/core';
 import {EventDto, EventsApiService} from "../../../core/api";
 import {ApplicationStateService} from "../../../core/services/application-state.service";
-import {EventApi, EventInput} from "@fullcalendar/core";
+import {EventInput} from "@fullcalendar/core";
+import {EventMapperService} from "./event-mapper.service";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EventsService {
 
   constructor(
     private readonly eventsApiService: EventsApiService,
-    private readonly state: ApplicationStateService
+    private readonly state: ApplicationStateService,
+    private readonly mapper: EventMapperService
   ) {
   }
 
   getEvents(startDate: Date, endDate: Date) {
     this.eventsApiService.getEvents(startDate.toISOString(), endDate.toISOString())
       .subscribe({
-        next: events => {
-          const eventInputs: EventInput[] = events.map(event => {
-            return {
-              id: event.id,
-              start: new Date(event.startDate),
-              end: new Date(event.endDate),
-              title: event.title,
-            }
-          });
+        next: (events: EventDto[]) => {
+          const eventInputs: EventInput[] = events.map(
+            (event: EventDto) => this.mapper.mapEventDtoToEventInput(event)
+          );
           this.state.patchState({events: eventInputs})
         }
       });
