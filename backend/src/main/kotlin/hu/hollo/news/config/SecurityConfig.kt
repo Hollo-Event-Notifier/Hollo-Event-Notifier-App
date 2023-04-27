be45fa9591ec27d509b1092dc776ac8a5f724235
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-27 12:44:39 +0200

    exclued GET /events endpoint from auth

--- backend/src/main/kotlin/hu/hollo/news/config/SecurityConfig.kt
+++ backend/src/main/kotlin/hu/hollo/news/config/SecurityConfig.kt
@@ -3,6 +3,7 @@ package hu.hollo.news.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
@@ -40,6 +41,7 @@ class SecurityConfig {
         }
         .authorizeHttpRequests {
             it.requestMatchers("/users/login").permitAll()
+            it.requestMatchers(HttpMethod.GET,"/events").permitAll()
             it.anyRequest().authenticated()
         }
         .oauth2ResourceServer {
