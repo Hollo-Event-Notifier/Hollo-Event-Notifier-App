8a269cfbdac19d58cd1dc4d9620bc99b34239d15
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-28 10:18:44 +0200

    added event-dto.type-guard

---
+++ frontend/src/app/features/event-display/utils/event-dto.type-guard.ts
@@ -0,0 +1,10 @@
+import {EventDto} from "../../../core/api";
+
+export function instanceOfEventDto(object: any): object is EventDto {
+  return 'title' in object &&
+    'place' in object &&
+    'organizer' in object &&
+    'hasPoints' in object &&
+    'startDate' in object &&
+    'endDate' in object
+}
