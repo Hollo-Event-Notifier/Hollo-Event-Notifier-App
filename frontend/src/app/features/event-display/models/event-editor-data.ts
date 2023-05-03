4aa0851eced7e46e9d90122b0bda4c66fbd2e313
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-05-03 23:04:07 +0200

    removed title fieldd from event-editor-data

--- frontend/src/app/features/event-display/models/event-editor-data.ts
+++ frontend/src/app/features/event-display/models/event-editor-data.ts
@@ -2,7 +2,6 @@ import {EventDto} from "../../../core/api";
 import {EventEditorMode} from "../enums/event-editor-mode";
 
 export interface EventEditorData {
-  title: string;
   event: EventDto;
   mode: EventEditorMode;
 }
