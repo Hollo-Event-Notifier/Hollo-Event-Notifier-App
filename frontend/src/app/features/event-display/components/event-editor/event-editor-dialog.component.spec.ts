f90f524cfb717db9dc75b8719596cc510487d0ed
vbkristof <122607148+vbkristof@users.noreply.github.com>
2023-05-03 23:11:08 +0200

    event crud part 2 (#32)
    * created event crud endpoints
    
    * renamed test
    
    * changed login path
    
    * api codegen
    
    * added event entities and modified filldatabase
    
    * renamed UsersController
    
    * added get events between dates endpoint
    
    * generated basic error response dto
    
    * api doc repair
    
    * api codegen
    
    * EventsController.getEventById endpoint
    
    * EventsController.createEvent endpoint
    
    * EventsController.updateEventById endpoint
    
    * EventsController.deleteEventById endpoint implementation
    
    * deleteEventById logic refactoring
    
    * api codegen
    
    * renamed UsersApiService
    
    * added default children path redirect to admin-routing.module
    
    * working state of grid concept
    
    * updatedd angular and added fullcalendar depenency
    
    * changed check-token endpoint to GET
    
    * BE codegen
    
    * fe codegen
    
    * corrected imports
    
    * add directives imports
    
    * new dates for mock data
    
    * exclued GET /events endpoint from auth
    
    * added events to app state
    
    * created events feature service
    
    * added full-calendar to event-display.component
    
    * Created matDialog. Challenges: 1) sending Event data to Dialog 2) proper html/css
    
    * changed state imports
    
    * added shared imports
    
    * changed event-display to module
    
    * created event mapper service
    
    * changed events.service to use mapper
    
    * moved event editor
    
    * changed event-display to handle event dialog
    
    * implemented event-editor-dialog.component with typed reactive forms
    
    * added calendar event type guard
    
    * adapted application state to handle event CRUD
    
    * added event-dto.type-guard
    
    * changed mapper service signature and corrections
    
    * added update, delete, create to events.service
    
    * new fields in event-form
    
    * added event-editor-data model
    
    * addded craete and update actions to event handdlers
    
    * added new functionality to event editor
    
    * URL validation for event creating/modification
    
    * Delete button only appearing at event update. Might need a refactor to make the solution pretier.
    
    * refactored event editor dialog mode
    
    * cleaning and refactorings
    
    * deleted commented code
    
    * added events service unit tests
    
    * moved and added imports
    
    * removed title fieldd from event-editor-data
    
    * added unit tests for event display feature services
    
    * event-editor-dialog.component unit tests
    
    * removed unnecessary code
    
    * removed unit test
    
    ---------
    
    Co-authored-by: András Ferenczy <aferenczy.education@gmail.com>

--- frontend/src/app/features/event-display/components/event-editor/event-editor-dialog.component.spec.ts
+++ frontend/src/app/features/event-display/components/event-editor/event-editor-dialog.component.spec.ts
@@ -1,17 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {EventDto} from '../../../../core/api';
+import {EventEditorData} from '../../models/event-editor-data';
+import {EventEditorMode} from '../../enums/event-editor-mode';
+import {EventEditorDialogComponent} from './event-editor-dialog.component';
+import {MatDialogRef, MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { EventEditorDialogComponent } from './event-editor-dialog.component';
-
-describe('EventEditorComponent', () => {
+describe('EventEditorDialogComponent', () => {
   let component: EventEditorDialogComponent;
   let fixture: ComponentFixture<EventEditorDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EventEditorDialogComponent ]
-    })
-    .compileComponents();
+  const eventEditorDataMock: EventEditorData = {
+    event: {
+      id: '123',
+      title: 'Test Title',
+      place: 'Test Place',
+      organizer: 'Test Organizer',
+      hasPoints: true,
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+      link: 'https://example.com',
+    },
+    mode: EventEditorMode.Create
+  }
 
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [EventEditorDialogComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef, useValue: {
+            close: jasmine.createSpy('close')
+          }
+        },
+        {provide: MAT_DIALOG_DATA, useValue: eventEditorDataMock}
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(EventEditorDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +60,69 @@ describe('EventEditorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form group with values from input data', () => {
+    // Arrange
+    const expectedFormValues: EventDto = {
+      title: eventEditorDataMock.event.title,
+      place: eventEditorDataMock.event.place,
+      organizer: eventEditorDataMock.event.organizer,
+      hasPoints: eventEditorDataMock.event.hasPoints,
+      startDate: eventEditorDataMock.event.startDate,
+      endDate: eventEditorDataMock.event.endDate,
+      link: eventEditorDataMock.event.link
+    };
+
+    // Act
+
+    // Assert
+    expect(component.formGroup.value).toEqual(expectedFormValues);
+  });
+
+  it('should emit updated event data on save', () => {
+    // Arrange
+    const expectedEventDto = {
+      id: '123',
+      title: 'New Title',
+      place: 'New Place',
+      organizer: 'New Organizer',
+      hasPoints: false,
+      startDate: eventEditorDataMock.event.startDate,
+      endDate: eventEditorDataMock.event.endDate,
+      link: eventEditorDataMock.event.link
+    } as EventDto;
+
+    // Act
+    component.formGroup.patchValue({
+      title: 'New Title',
+      place: 'New Place',
+      organizer: 'New Organizer',
+      hasPoints: false
+    });
+    component.onSave();
+
+    // Assert
+    expect(component['dialogRef'].close).toHaveBeenCalledWith(expectedEventDto);
+  });
+
+  it('should emit event id on delete', () => {
+    // Arrange: done in beforeEach
+
+    // Act
+    component.onDelete();
+
+    // Assert
+    expect(component['dialogRef'].close).toHaveBeenCalledWith('123');
+  });
+
+  it('should close dialog on cancel', () => {
+    // Arrange: done in beforeEach
+
+    // Act
+    component.onCancel();
+
+    // Assert
+    expect(component['dialogRef'].close).toHaveBeenCalled();
   });
 });
---
+++ frontend/src/app/features/event-display/services/event-mapper.service.spec.ts
@@ -0,0 +1,75 @@
+import {TestBed} from '@angular/core/testing';
+import {EventMapperService} from './event-mapper.service';
+import {EventDto} from "../../../core/api";
+import {EventInput} from "@fullcalendar/core";
+
+describe('EventMapperService', () => {
+  let underTest: EventMapperService;
+
+  beforeEach(() => {
+    underTest = new EventMapperService();
+  });
+
+  it('should map EventDto to EventInput', () => {
+    // Arrange
+    const dto: EventDto = {
+      id: '1',
+      startDate: '2022-05-01T09:00:00Z',
+      endDate: '2022-05-01T10:00:00Z',
+      title: 'Test Event',
+      place: 'Test Place',
+      organizer: 'Test Organizer',
+      hasPoints: true,
+      link: 'http://test.com',
+    };
+    const expected: EventInput = {
+      id: '1',
+      start: new Date('2022-05-01T09:00:00Z'),
+      end: new Date('2022-05-01T10:00:00Z'),
+      title: 'Test Event',
+      extendedProps: {
+        place: 'Test Place',
+        organizer: 'Test Organizer',
+        hasPoints: true,
+        link: 'http://test.com',
+      }
+    };
+
+    // Act
+    const result = underTest.mapEventDtoToCalendarEvent(dto);
+
+    // Assert
+    expect(result).toEqual(expected);
+  });
+
+  it('should map calendar event to DTO', () => {
+    // Arrange
+    const input: EventInput = {
+      id: '1',
+      start: new Date('2022-05-02T14:30:00.000Z'),
+      end: new Date('2022-05-02T16:30:00.000Z'),
+      title: 'Test Event',
+      extendedProps: {
+        place: 'Test Place',
+        organizer: 'Test Organizer',
+        hasPoints: true,
+        link: 'https://example.com',
+      },
+    };
+
+    // Act
+    const result: EventDto = underTest.mapCalendarEventToEventDto(input);
+
+    // Assert
+    expect(result).toEqual({
+      title: 'Test Event',
+      place: 'Test Place',
+      organizer: 'Test Organizer',
+      hasPoints: true,
+      startDate: '2022-05-02T14:30:00.000Z',
+      endDate: '2022-05-02T16:30:00.000Z',
+      id: '1',
+      link: 'https://example.com',
+    });
+  });
+});
