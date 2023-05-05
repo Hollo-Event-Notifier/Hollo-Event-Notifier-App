d128a20a966f447a086821117d709d2329445c75
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-05-05 12:58:07 +0200

    creaated full-calendar-wrapper.component in shared.module

---
+++ frontend/src/app/shared/components/full-calendar-wrapper/full-calendar-wrapper.component.ts
@@ -0,0 +1,100 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventAddArg,
+  EventChangeArg,
+  EventClickArg, EventInput,
+  EventRemoveArg
+} from "@fullcalendar/core";
+import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import {EventDto} from "../../../core/api";
+import {EventMapperService} from "../../../core/services/event-mapper.service";
+
+// TODO: add change detection
+@Component({
+  selector: 'app-full-calendar-wrapper',
+  templateUrl: './full-calendar-wrapper.component.html',
+  styleUrls: ['./full-calendar-wrapper.component.scss']
+})
+export class FullCalendarWrapperComponent implements OnInit {
+  @Output('dateSelect') dateSelectEmitter = new EventEmitter<EventDto>();
+  @Output('eventClick') eventClickEmitter = new EventEmitter<EventDto>();
+  @Output('eventChange') eventChangeEmitter = new EventEmitter<EventDto>();
+  @Output('eventAdd') eventAddEmitter = new EventEmitter<EventDto>();
+  @Output('eventRemove') eventRemoveEmitter = new EventEmitter<EventDto>();
+
+  @Input() events: EventInput[] = [];
+  @Input() isEditable: boolean = false;
+  @Input() hasWeekends: boolean = false;
+  @Input() isSelectable: boolean = false;
+
+
+  calendarOptions: CalendarOptions = {
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'timeGridWeek',
+    selectMirror: true,
+    dayMaxEvents: true,
+    firstDay: 1,
+  };
+
+  constructor(private readonly eventMapperService: EventMapperService) {
+  }
+
+  ngOnInit(): void {
+    this.calendarOptions = {
+      ...this.calendarOptions,
+      weekends: this.hasWeekends,
+      editable: this.isEditable,
+      selectable: this.isSelectable,
+      select: this.handleDateSelect.bind(this),
+      eventClick: this.handleEventClick.bind(this),
+      eventChange: this.handleEventChange.bind(this),
+      eventAdd: this.handleEventAdd.bind(this),
+      eventRemove: this.handleEventRemove.bind(this),
+    }
+  }
+
+  private handleDateSelect(selectInfo: DateSelectArg): void {
+    this.dateSelectEmitter.emit({
+      startDate: selectInfo.start.toISOString(),
+      endDate: selectInfo.end.toISOString(),
+      link: '',
+      place: '',
+      title: '',
+      organizer: '',
+      hasPoints: false
+    });
+  }
+
+  private handleEventClick(clickInfo: EventClickArg): void {
+    this.eventClickEmitter.emit(
+      this.eventMapperService.mapCalendarEventToEventDto(clickInfo.event)
+    );
+  }
+
+  private handleEventChange(changeInfo: EventChangeArg) {
+    this.eventChangeEmitter.emit(
+      this.eventMapperService.mapCalendarEventToEventDto(changeInfo.event)
+    );
+  }
+
+  private handleEventAdd(addInfo: EventAddArg): void {
+  }
+
+  private handleEventRemove(removeInfo: EventRemoveArg): void {
+  }
+}
