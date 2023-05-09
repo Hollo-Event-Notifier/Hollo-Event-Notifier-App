b1ea1dac527b1de2fae40addd3ead22e35babd19
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-05-09 12:37:58 +0200

    removed unnecessary code

--- frontend/src/app/features/event-display/event-display.component.ts
+++ frontend/src/app/features/event-display/event-display.component.ts
@@ -6,22 +6,13 @@ import {EventInput} from "@fullcalendar/core";
 import {ApplicationStateService} from "../../core/services/application-state.service";
 import {EventsService} from "../../core/services/events.service";
 import {MatDialog} from "@angular/material/dialog";
-import {EventEditorDialogComponent} from "../admin/components/event-editor/event-editor-dialog.component";
-import {EventEditorMode} from "../admin/enums/event-editor-mode";
-import {EventEditorData} from "../admin/models/event-editor-data";
-import {instanceOfEventDto} from "../../core/utils/event-dto.type-guard";
 import {EventDisplayDialogComponent} from "./components/event-display/event-display-dialog.component";
-
-interface EventDisplayDialogData {
-}
 
 @Component({
   selector: 'app-event-display',
   templateUrl: './event-display.component.html',
   styleUrls: ['./event-display.component.scss'],
-  imports: [
-    SharedModule,
-  ],
+  imports: [SharedModule,],
   standalone: true
 })
 export class EventDisplayComponent {
