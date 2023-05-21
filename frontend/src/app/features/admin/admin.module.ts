5583754d173d15317fdfd134b73a5e3644e807f9
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-05-21 23:43:06 +0200

    added imports and declarations for admin.module

--- frontend/src/app/features/admin/admin.module.ts
+++ frontend/src/app/features/admin/admin.module.ts
@@ -1,16 +1,23 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Provider} from '@angular/core';
 import {AdminRootComponent} from './components/admin-root/admin-root.component';
 import {AdminRoutingModule} from "./admin-routing.module";
 import {SharedModule} from "../../shared/shared.module";
-import { SettingsComponent } from './components/settings/settings.component';
-import { AdminEventDisplayComponent } from './components/admin-event-display/admin-event-display.component';
-import {EventEditorDialogComponent} from "./components/event-editor/event-editor-dialog.component";
+import {SettingsComponent} from './components/settings/settings.component';
+import {AdminEventDisplayComponent} from './components/admin-event-display/admin-event-display.component';
+import {EventEditorDialogComponent} from "./components/event-editor-dialog/event-editor-dialog.component";
+import {UsersService} from "./services/users.service";
+import {UserDeleteDialogComponent} from './components/user-delete-dialog/user-delete-dialog.component';
+import {UserEditorDialogComponent} from './components/user-editor-dialog/user-editor-dialog.component';
+import {UserCreatorDialogComponent} from './components/user-creator-dialog/user-creator-dialog.component';
 
 const DECLARATIONS = [
   AdminRootComponent,
   SettingsComponent,
   AdminEventDisplayComponent,
-  EventEditorDialogComponent
+  EventEditorDialogComponent,
+  UserDeleteDialogComponent,
+  UserEditorDialogComponent,
+  UserCreatorDialogComponent,
 ];
 
 const IMPORTS = [
@@ -18,9 +25,12 @@ const IMPORTS = [
   SharedModule
 ];
 
+const PROVIDERS: Provider[] = [UsersService]
+
 @NgModule({
   declarations: [...DECLARATIONS],
-  imports: [...IMPORTS]
+  imports: [...IMPORTS],
+  providers: [...PROVIDERS]
 })
 export class AdminModule {
 }
