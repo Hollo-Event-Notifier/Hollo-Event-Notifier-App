9d56f2f1c96c0fa640375c0dcbb7a76da10292af
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-05-21 23:40:21 +0200

    FE api codegen

--- frontend/src/app/core/api/model/models.ts
+++ frontend/src/app/core/api/model/models.ts
@@ -1,3 +1,6 @@
+export * from './create-user-request-all-of-dto';
+export * from './create-user-request-dto';
 export * from './error-dto';
 export * from './event-dto';
 export * from './user-credentials-dto';
+export * from './user-dto';
