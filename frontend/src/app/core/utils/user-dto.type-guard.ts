13a6504a19b5ce99d668efe31f0d443ab32bf8b6
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-05-22 11:37:37 +0200

    created and removed type guards

--- frontend/src/app/core/utils/user-dto.type-guard.ts
+++
@@ -1,7 +0,0 @@
-import {UserDto} from "../api";
-
-export function instanceOfUserDto(object: object): object is UserDto {
-  return 'username' in object &&
-    'email' in object &&
-    'id' in object;
-}
