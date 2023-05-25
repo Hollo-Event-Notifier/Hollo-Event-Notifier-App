af9bc58e811375b9fd299e286d7089161984e0e7
András Ferenczy <aferenczy.education@gmail.com>
2023-05-25 11:47:24 +0200

    New service merged into already existing one

--- frontend/src/app/core/services/events.service.ts
+++ frontend/src/app/core/services/events.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {EventDto, EventsApiService} from "../api";
 import {ApplicationStateService} from "./application-state.service";
-import {EventInput} from "@fullcalendar/core";
+import {Calendar, EventInput} from "@fullcalendar/core";
 import {EventMapperService} from "./event-mapper.service";
 import {SnackbarService} from "./snackbar.service";
 
@@ -26,6 +26,39 @@ export class EventsService {
           this.state.patchState({events: eventInputs})
         }
       });
+  }
+
+  getEventsForCurrentView(calendar: Calendar): void {
+    const currentView = calendar.view;
+
+    const currentDate: Date = new Date();
+    const currentMonthStart: Date = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const currentMonthEnd: Date = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+    const previousMonthStart: Date = new Date(currentMonthStart.getFullYear(), currentMonthStart.getMonth() - 1, 1);
+    const previousMonthEnd: Date = new Date(currentMonthStart.getFullYear(), currentMonthStart.getMonth(), 0);
+    const nextMonthStart: Date = new Date(currentMonthEnd.getFullYear(), currentMonthEnd.getMonth() + 1, 1);
+    const nextMonthEnd: Date = new Date(currentMonthEnd.getFullYear(), currentMonthEnd.getMonth() + 2, 0);
+
+    let startDate: Date;
+    let endDate: Date;
+
+    switch (currentView.type) {
+      case 'dayGridMonth':
+      case 'timeGridWeek':
+        startDate = currentMonthStart;
+        endDate = currentMonthEnd;
+        break;
+      case 'timeGridDay':
+        startDate = currentView.currentStart as Date;
+        endDate = currentView.currentEnd as Date;
+        break;
+      default:
+        startDate = previousMonthStart;
+        endDate = nextMonthEnd;
+        break;
+    }
+
+    this.getEvents(startDate, endDate);
   }
 
   updateEvent(eventToUpdate: EventDto): void {
